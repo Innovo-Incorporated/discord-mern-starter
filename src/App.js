4894994d8a31e4eda54357989d183f25e99a2173
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-11-27 12:34:30 +0530

    fixed breaking issue in site

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Sidebar';
-import ExpandMore from '@material-ui/icons/ExpandMore'
-import Chat from './Chat';
-import { selectUser } from './features/userSlice';
-import { useDispatch, useSelector } from 'react-redux'
-import Login from './Login';
-import { useEffect } from 'react';
-import { auth } from './firebase';
-import { login, logout } from './features/userSlice'
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+// import ExpandMore from '@material-ui/icons/ExpandMore' not used
+import Chat from "./Chat";
+import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "./Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
-  const dispatch = useDispatch()
-  const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-
-      console.log(authUser)
+      console.log(authUser);
 
       if (authUser) {
-        dispatch(login({
-          uid: authUser.uid,
-          photo: authUser.photoURL,
-          email: authUser.email,
-          displayName: authUser.displayName
-        }))
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
       } else {
-        dispatch(logout())
+        dispatch(logout());
       }
-    })
-  }, [dispatch])
+    });
+  }, [dispatch]);
 
-  console.log(user)
+  console.log(user);
 
   return (
     <div className="app">
@@ -41,10 +42,9 @@ function App() {
           <Sidebar />
           <Chat />
         </>
-
       ) : (
-          <Login />
-        )}
+        <Login />
+      )}
     </div>
   );
 }
