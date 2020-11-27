b74545eb177ee07856c65761b2195c8d0a9a4125
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-11-27 12:53:12 +0530

    fixing stuff

---
+++ build/precache-manifest.9a0ead6771c9daeb5e94319f7c6b8ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc05e63320de8bf4096d7b67bbfd92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "739c5a0ca647dbd470e8",
+    "url": "/static/css/main.630c6eb0.chunk.css"
+  },
+  {
+    "revision": "a162b182909b153a6c26",
+    "url": "/static/js/2.b7cf6473.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.b7cf6473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739c5a0ca647dbd470e8",
+    "url": "/static/js/main.e5c2e65c.chunk.js"
+  },
+  {
+    "revision": "fa430200eae998bbeca5",
+    "url": "/static/js/runtime-main.ee4f1273.js"
+  }
+]);
